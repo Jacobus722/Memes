291a72c46bc646c45634d23263ce8d3a60625c6b
Jacobus722 <34773576+Jacobus722@users.noreply.github.com>
2017-12-26 11:26:32 -0600

    Add files via upload

---
+++ rainbow.js
@@ -0,0 +1,15 @@
+(function() {
+	var self = {
+		var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+		var i = 1;
+
+		window.setInterval(function(){
+			document.body.style.backgroundColor = colors[i];
+			i++;
+			if (i === colors.length){
+				i=0;
+			}
+		}, 100);
+	}
+}
